00e43e014244dfd4c8057e306e3e91729b9ad6cf
CharlesMich <charleedongo@hotmail.com>
2025-02-17 10:18:06 -0600

    Initial Commit

---
+++ bible_talk/bible_talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  bible_talkApp.swift
+//  bible_talk
+//
+//  Created by Charles Michael on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct bible_talkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
